4ba11aac6b5154ceee903d7b2ecdc4496bcc4b9f
da-Kai <kaijeschek+github@gmail.com>
2024-03-03 20:30:52 +0100

    quickfix

--- tools/docker/edge/ui/assets/env.template.js
+++ tools/docker/edge/ui/assets/env.template.js
@@ -2,5 +2,5 @@
     window[\"env\"] = window[\"env\"] || {};
   
     // Environment variables
-    window[\"env\"][\"websocket\"] = \"ws://${EDGE_HOST:-127.0.0.1}:${EDGE_REST_API_PORT:-8085}\";
+    window[\"env\"][\"websocket\"] = \"ws://${EDGE_HOST:-127.0.0.1}:${EDGE_WEBSOCKET_PORT:-8085}\";
 })(this);
