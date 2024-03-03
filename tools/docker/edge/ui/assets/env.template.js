0a25da6946a80bdee44df28c459ca25d6f58577d
da-Kai <kaijeschek+github@gmail.com>
2024-03-03 20:11:27 +0100

    env file

--- tools/docker/edge/ui/assets/env.template.js
+++ tools/docker/edge/ui/assets/env.template.js
@@ -1,6 +1,6 @@
 (function(window) {
-    window["env"] = window["env"] || {};
+    window[\"env\"] = window[\"env\"] || {};
   
     // Environment variables
-    window["env"]["websocket"] = "ws://${EDGE_HOST:-127.0.0.1}:${EDGE_REST_API_PORT:-8085}";
+    window[\"env\"][\"websocket\"] = \"ws://${EDGE_HOST:-127.0.0.1}:${EDGE_REST_API_PORT:-8085}\";
 })(this);
