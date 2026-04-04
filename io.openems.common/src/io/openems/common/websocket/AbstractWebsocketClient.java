33c01819eab1d31bbd1e68180f859eacba53ad6c
da Kai <da.kai.creator@gmail.com>
2026-04-04 17:44:20 +0200

    fix debugLog

--- io.openems.common/src/io/openems/common/websocket/AbstractWebsocketClient.java
+++ io.openems.common/src/io/openems/common/websocket/AbstractWebsocketClient.java
@@ -256,8 +256,10 @@ public abstract class AbstractWebsocketClient<T extends WsData> extends Abstract
 	 * @return the debug log output or null
 	 */
 	public String debugLog() {
-		var b = new StringBuilder("[").append(this.getName()).append("] [monitor] ");
-		if (this.ws.get().isOpen()) {
+		var b = new StringBuilder(64) //
+				.append("[").append(this.getName()).append("] [monitor] ");
+		final var websocket = this.ws.get();
+		if (websocket != null && websocket.isOpen()) {
 			b.append("Connected ");
 		} else {
 			b.append("NOT CONNECTED. ").append(this.reconnectorWorker.debugLog());
